5f16c8e7da5b21d59386837b30e708c194587ce8
mickrueg <mickrueg@gmail.com>
2022-08-04 16:02:15 -0600

    React app installed and basic css set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import sparkles from './images/sparkles.png'
+
+function App() {
+  return (
+    <div className='App'>
+      <img className='sparkle-one' src={sparkles} alt='Sparkle to add depth' />
+      
+      <img className='sparkle-two' src={sparkles} alt='Sparkle to add depth' />
+    </div>
+  );
+}
+
+export default App;
