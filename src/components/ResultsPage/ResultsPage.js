20bc17ddd044f093793d4dcb15fd029e5a021cfb
time2fishman <adam_h_martinez@hotmail.com>
2022-08-09 15:16:25 -0600

    progress on results page

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -4,8 +4,32 @@ import './ResultsPage.css';
 import Option from '../Option/Option';
 
 const ResultsPage = () => {
+
+    const { finalResult, playArray, navBarVisible, setNavBarVisible } = useContext(AppContext)
+    const chosenFood = playArray[finalResult].name
+
+    useEffect(() => {
+        setNavBarVisible('NavBar-container')
+    })
+
     return (
-        <div className='resultsPage-main'>test</div>
+        <main className='resultsPage-main'>
+            <div className='resultsPage-background'>
+                <div className='resultsPage-container'>
+                    <div className='results-info-box'>
+                        <h2>The universe has spoken! Somebody's eatin' some "{chosenFood}"!</h2>
+                    </div>
+                    <div className='food-result'>
+                        <Option
+                            name={playArray[finalResult].name}
+                            image={playArray[finalResult].image}
+                            cuisine={playArray[finalResult].cuisine}
+                            description={playArray[finalResult].description}
+                        />
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
 
