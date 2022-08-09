9aa6a7139a4707f56f13ac61a2548000b8d59f85
time2fishman <adam_h_martinez@hotmail.com>
2022-08-09 11:10:46 -0600

    started resultsPage

---
+++ src/components/ResultsPage/ResultsPage.js
@@ -0,0 +1,12 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../../AppContext';
+import './ResultsPage.css';
+import Option from '../Option/Option';
+
+const ResultsPage = () => {
+    return (
+        <div className='resultsPage-main'>test</div>
+    )
+}
+
+export default ResultsPage;
